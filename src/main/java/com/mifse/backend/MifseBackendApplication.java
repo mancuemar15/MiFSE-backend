34e0a72cf7a4ae5a8e465e15152ee779ad8ea291
mancuemar15 <mancuemar15@gmail.com>
2023-04-11 22:42:25 +0200

    Proyecto creado

---
+++ src/main/java/com/mifse/backend/MifseBackendApplication.java
@@ -0,0 +1,13 @@
+package com.mifse.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MifseBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MifseBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/mifse/backend/MifseBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mifse.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MifseBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
