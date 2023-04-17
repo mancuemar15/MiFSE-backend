b1713a78c8b777f775991f95ee1efcc19095f3e1
mancuemar15 <mancuemar15@gmail.com>
2023-04-17 22:19:31 +0200

    Configurado encriptador y desactivado csrf token

--- src/main/java/com/mifse/backend/MifseBackendApplication.java
+++ src/main/java/com/mifse/backend/MifseBackendApplication.java
@@ -2,9 +2,25 @@ package com.mifse.backend;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
 
 @SpringBootApplication
 public class MifseBackendApplication {
+
+	@Bean
+	public PasswordEncoder passwordEncoder() {
+		return new BCryptPasswordEncoder();
+	}
+
+	@Bean
+	public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+		http.csrf().disable();
+		return http.build();
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(MifseBackendApplication.class, args);
