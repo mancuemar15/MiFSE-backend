3905d6789d1903700cf5a23a3b8370f47d7c9dbe
mancuemar15 <mancuemar15@gmail.com>
2023-04-17 21:31:07 +0200

    Creado modelo Lista

---
+++ src/main/java/com/mifse/backend/persistencia/modelos/Lista.java
@@ -0,0 +1,36 @@
+package com.mifse.backend.persistencia.modelos;
+
+import java.time.LocalDateTime;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
+
+import lombok.Data;
+
+@Data
+@Entity
+@Table(name = "lista")
+public class Lista {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column(name = "ID")
+	private Integer id;
+
+	@ManyToOne
+	@JoinColumn(name = "ID_residente", nullable = false)
+	private Residente residente;
+
+	@Column(name = "Fecha_creacion", nullable = false)
+	private LocalDateTime fechaCreacion;
+
+	@Column(name = "Fecha_actualizacion", nullable = false)
+	private LocalDateTime fechaActualizacion;
+
+}
