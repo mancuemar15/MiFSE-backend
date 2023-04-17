a68bf0a96fd680acf2c24f8e1336d1aaa92c63df
mancuemar15 <mancuemar15@gmail.com>
2023-04-17 21:13:46 +0200

    Creado modelo Provincia

--- src/main/java/com/mifse/backend/persistencia/modelos/Provincia.java
+++ src/main/java/com/mifse/backend/persistencia/modelos/Provincia.java
@@ -2,21 +2,29 @@ package com.mifse.backend.persistencia.modelos;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
 import javax.persistence.Table;
 
 import lombok.Data;
 
 @Data
 @Entity
-@Table(name = "autonomia")
-public class Autonomia {
+@Table(name = "provincia")
+public class Provincia {
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column(name = "ID")
 	private Integer id;
 
-	@Column(name = "Nombre", nullable = false)
+	@Column(name = "Nombre")
 	private String nombre;
+
+	@ManyToOne(fetch = FetchType.LAZY)
+	@JoinColumn(name = "ID_autonomia", referencedColumnName = "ID")
+	private Autonomia autonomia;
 }
