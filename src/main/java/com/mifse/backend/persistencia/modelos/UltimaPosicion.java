a867b6bc71181534bb29eac0a10527737116f618
mancuemar15 <mancuemar15@gmail.com>
2023-04-17 20:56:27 +0200

    Creado modelo UltimaPosicion

---
+++ src/main/java/com/mifse/backend/persistencia/modelos/UltimaPosicion.java
@@ -0,0 +1,35 @@
+package com.mifse.backend.persistencia.modelos;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
+
+import lombok.Data;
+
+@Data
+@Entity
+@Table(name = "ultima_posicion")
+public class UltimaPosicion {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column(name = "ID")
+	private Integer id;
+
+	@Column(name = "Numero")
+	private Integer numero;
+
+	@Column(name = "Ano")
+	private Integer ano;
+
+	@ManyToOne(fetch = FetchType.LAZY)
+	@JoinColumn(name = "ID_especialidad")
+	private Especialidad especialidad;
+
+}
