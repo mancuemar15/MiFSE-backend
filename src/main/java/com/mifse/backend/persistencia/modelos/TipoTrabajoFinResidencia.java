672286fc77e0b83525521c96d04a7321dc7564b5
mancuemar15 <mancuemar15@gmail.com>
2023-04-17 21:26:02 +0200

    Creado modelo import javax.persistence.Column;

---
+++ src/main/java/com/mifse/backend/persistencia/modelos/TipoTrabajoFinResidencia.java
@@ -0,0 +1,25 @@
+package com.mifse.backend.persistencia.modelos;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+import lombok.Data;
+
+@Data
+@Entity
+@Table(name = "tipo_trabajo_fin_residencia")
+public class TipoTrabajoFinResidencia {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column(name = "ID")
+	private int id;
+
+	@Column(name = "Tipo")
+	private String tipo;
+
+}
